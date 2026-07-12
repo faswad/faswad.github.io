6a652402380feac6912fb62787044120cfa49d72
Firas Aswad <faswad@uomosul.edu.iq>
2026-07-12 17:00:12 +0300

    Update_config.js

--- salat/assets/js/config.js
+++ salat/assets/js/config.js
@@ -1,8 +1,13 @@
 window.APP_CONFIG = Object.freeze({
   version: "1.0.0",
-  apkUrl: "https://github.com/faswad/firas-salat-app/releases/latest/download/firas-prayer-display.apk",
+
+  apkUrl: "https://github.com/faswad/faswad.github.io/releases/latest/download/firas-prayer-display.apk",
+
   apkFileName: "firas-prayer-display.apk",
+
   sha256: "f313757ab96c3212bcb657098163428c8b9d346c68383c44486521ed53aed424",
+
   apkSize: "61.3 MB",
+
   developer: "Dr. Firas Mohammed Salh Whab Aswad"
 });
