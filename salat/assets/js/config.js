31e456b879169440a78cdb7b9d398a187893020b
Firas Aswad <faswad@uomosul.edu.iq>
2026-07-21 15:31:23 +0300

    Update Website and APK

--- salat/assets/js/config.js
+++ salat/assets/js/config.js
@@ -1,8 +1,8 @@
 window.APP_CONFIG = Object.freeze({
-  version: "2.6.5",
-  buildNumber: "265",
+  version: "1.0.0",
+  buildNumber: "1",
   apkUrl: "assets/downloads/firas-prayer-display.apk",
   apkFileName: "firas-prayer-display.apk",
   apkSize: "34.8 MB",
-  sha256: "97b531bd60d97a34689d674786c8a1ef06d0804f8967babce5a11b340bb10dba"
+  sha256: "7ac9a47cb4c3dcdf24f126ed82d52bfb8fa2f5b896ef390e1e630fffd9db74c9"
 });
